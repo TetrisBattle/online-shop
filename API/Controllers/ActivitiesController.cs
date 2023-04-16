25683833c5bc533e48c11ed7d20c3b2102780368
TetrisBattle <thien5596@gmail.com>
2023-04-16 14:59:00 +0300

    Add createActivity and EditActivity Add AutoMapper

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,7 +7,6 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-
 	[HttpGet]
 	public async Task<ActionResult<List<Activity>>> GetActivities()
 	{
@@ -18,5 +17,18 @@ public class ActivitiesController : BaseApiController
 	public async Task<ActionResult<Activity>> GetActivity(Guid id)
 	{
 		return await Mediator.Send(new Details.Query { Id = id });
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> CreateActivity(Activity activity)
+	{
+		return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+	}
+
+	[HttpPut("{id}")]
+	public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+	{
+		activity.Id = id;
+		return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
 	}
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Application.Core;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -21,6 +22,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000")));
 
 builder.Services.AddMediatR(typeof(List.Handler).Assembly);
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
